79520585b8ad010eeb7a8dba8e85e1d8a217065f
Greg Gannicott <greg@gannicott.co.uk>
2026-09-02 17:10:38 +0100

    chore: Reword to make easier to find

--- commands/worktree-from-pr.sh
+++ commands/worktree-from-pr.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-printf '%s\n' '{"type":"worktree-from-pr","label":"Open Worktree for PRs in Current Repo"}'
+printf '%s\n' '{"type":"worktree-from-pr","label":"Open Worktree for Code Review in Current Repo"}'
